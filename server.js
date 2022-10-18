52dff6a60aed05d71eadad5fb7d5c2d01062f461
Maximiliano <biayo79@gmail.com>
2022-10-18 22:46:52 +0200

    added user creation functionality

--- server.js
+++ server.js
@@ -3,12 +3,43 @@ const { config } = require('dotenv')
 const express = require('express')
 const app = express()
 
+const bcrypt = require('bcrypt')
+
 config()
 
+const users = []
+
+// @REMEMBER: allows app to receive json in the request
 app.use(express.json())
 
+// TODO: remove this route
 app.get('/', (req, res) => {
     res.send('Hello world')
+})
+
+// TODO: authorize this route
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+// TODO: authorize this route
+app.post('/users', async (req, res) => {
+    try {
+        // create salt & password
+        const salt = await bcrypt.genSalt()
+        const password = await bcrypt.hash(req.body.password, salt)
+
+        // create user
+        const user = { name: req.body.name, password: password }
+
+        // hash password
+        // persist user
+        users.push(user)
+    } catch (error) {
+        res.status(500).send()
+    }
+
+    res.status(201).send()
 })
 
 const PORT = parseInt(process.env.PORT) || 3000
